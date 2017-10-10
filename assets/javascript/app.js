2577bea2ed21f4e64136e8b0931567d2ca00271b
Bryan Isles <bryanisles@gmail.com>
2017-10-10 18:36:54 -0500

    Final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,47 @@
-
 // listing all global variables
 var myQuestions = [{
 	// question 1
-	myQuestion:"Mathematically @ standard conditions, what is the relative mass of Earth to itself @ standard conditions?",
-	myAnswers: [1.0 ,0.5 ,0.88 ,1.23],
-	myCorrectAnswer: 1.0// myQuestions[0].myAnswers[0]
+	myQuestion:"How many colors are there in the spectrum when white light is separated?",
+	myAnswers: [5.0 ,7.0 ,4.5 ,3.2],
+	myCorrectAnswer: 7.0// myQuestions[0].myAnswers[1]
 },{
 	// question 2
 	myQuestion:"An example of a non-newtonian fluid is?",
-	myAnswers:["oobleck","milk", "water", "pitch"],
+	myAnswers:["milk", "water", "pitch","oobleck"],
 	myCorrectAnswer: "oobleck" //myQuestions[1].myAnswers[0]
 },{
 	// question 3
-	myQuestion:"How much force is applied against us from the ground?",
-	myAnswers:["equal to our weight", "equal to the earth's weight", "equal to the gravitation attractive force","equal to the earth's mass times earth's gravitation constant"],
-	myCorrectAnswer: "equal to our weight" //myQuestions[2].myAnswers[0]
+	myQuestion:"What is the force that opposes the relative motion of two bodies that are in contact?",
+	myAnswers:["Gravity","Tension","Friction","Significant-Other"],
+	myCorrectAnswer: "Friction" //myQuestions[2].myAnswers[0]
 },{
 	// question 4
 	myQuestion:"On a clear cloudless day, why is the appearance of the sky blue?",
-	myAnswers:["It's the reflection of the ocean", "It is due to the atmospheric index of refraction", "Due to someone shining a blue light towards the sky during the day","There is some chemical aerosol that is dispersed in the atmosphere that gives it that appearance"],
-	myCorrectAnswer: "It is due to the atmospheric index of refraction" //myQuestions[3].myAnswers[1]
+	myAnswers:["Oceanic Reflection", "Atmospheric Refractive Properties", "Blue LED light","Chemical Aerosol"],
+	myCorrectAnswer: "Atmospheric Refractive Properties" //myQuestions[3].myAnswers[1]
+},{
+	// question 5
+	myQuestion:"What is the product of the mass of a body and its linear velocity",
+	myAnswers:["Momentum","Force","Energy","Weight"],
+	myCorrectAnswer: "Momentum"
+},{
+	// question 6
+	myQuestion:"A derivative represents the rate of change of something.  What is the derivative of a velocity function with respect to time?",
+	myAnswers:["Velocity","Jerk","Acceleration","Distance"],
+	myCorrectAnswer: "Acceleration"
+},{
+	// question 7
+	myQuestion:"What describes a substance that exists in more than one form, differing in physical rather than chemical properties?",
+	myAnswers:["Supercritical","Allotropic","Two-Phase fluids","Unobserved particle"],
+	myCorrectAnswer: "Allotropic"
+},{
+	// question 8
+	myQuestion:"What does c represent in the equation e=mc^2?",
+	myAnswers:["Speed of Light", "Celsius","Capacitance","Convergence"],
+	myCorrectAnswer: "Speed of Light"
 }];
 
+var timer;
 var myFirstTime = 120;
 var myIter = 0;
 var myZero = 0;
@@ -36,7 +56,7 @@ var onClickStart = function() {
 var userConfirmation = function() {
 	var tempConfirm = confirm("play again");
 	if(tempConfirm){
-			myCont.myClearFunc();	
+		myCont.myClearFunc();	
 	}
 };
 
@@ -45,7 +65,9 @@ var myCont = {
 	myIncorrectCounter: myZero,
 	myCounter: myFirstTime,
 // ===================================================================================================
-/*	mySelectedQuestions:[],
+/* Working code for discretizing questions
+
+	mySelectedQuestions:[],
 	myCheckAnsweredQuestions: function(){
 		do{
 			myIter++;
@@ -68,30 +90,54 @@ var myCont = {
 	// countdown function
 	myCountdown: function() {
 		myCont.myCounter--;
-		$("#myCounter").html(myCont.myCounter);
+		$("#myCounter").html(myCont.myCounterConversion);
 		if(myCont.myCounter <= 0){
 			console.log("Time is up");
 			myCont.myCompletion();
 		}
 	},
+	myCounterConversion: function(){
+		var myMinutes = Math.floor(myCont.myCounter / 60);
+		var mySeconds = myCont.myCounter % 60;
+
+		var myMinStr = myMinutes.toString();
+		var mySecStr = mySeconds.toString();
+
+		var tempMin;
+		var tempSec;
+
+		if(myMinStr.length == 1){
+			tempMin = "0" + myMinStr;
+		} else {
+			tempMin = myMinStr;
+		}
+
+		if(mySecStr.length == 1) {
+			tempSec = "0" + mySecStr;
+		} else {
+			tempSec = mySecStr;
+		}
+
+		return tempMin + ":" + tempSec;
+	},
 	//initialize function
 	myStart: function() {
-		var timer = setInterval(myCont.myCountdown, 1000);
-		$("#subwrapper").prepend("<h3>Time: <span id='myCounter'>" 
-			                      + myCont.myCounter
-			                      + "</span> Seconds</h3>")
+		timer = setInterval(myCont.myCountdown, 1000);
+		$("#subwrapper").prepend("<h3>Time: <span id='myCounter' style='font-family:consolas;'>" 
+			                      + myCont.myCounterConversion()
+			                      + "</span></h3>")
 		$("#start").remove();
 		for(var i = 0; i < myQuestions.length; i++) {
-			$("#subwrapper").append("<h2>" 
+			$("#subwrapper").append("<h3>" 
 				                     + myQuestions[i].myQuestion 
-				                     + "</h2>")
+				                     + "</h3>")
 			for(var j = 0; j < myQuestions[i].myAnswers.length; j++) {
 				$("#subwrapper").append("<input type='radio' name='myQuestion-" 
 					                    + i 
 					                    + "' value='"
 					                    + myQuestions[i].myAnswers[j]
-					                    +"'>" 
-					                    + myQuestions[i].myAnswers[j]);
+					                    +"'><h4 style='display:inline;padding:0 5px;'>" 
+					                    + myQuestions[i].myAnswers[j])+"</h4> ";
 			}
 		}
 	},
@@ -115,6 +161,7 @@ var myCont = {
 		clearInterval(timer);
 		$("#subwrapper h2").remove();
 		$("#subwrapper").html("Game Over");
+		var correctDiv
 		$("#subwrapper").append("<h3>Total Correct: " 
 			                     + this.myCorrectCounter 
 			                     +"</h3>");
